58ee55275af163ed1488aa51de60527853970b01
George Koodarappally <gjkood@yahoo.com>
2014-10-31 14:22:10 -0700

    Updated test data generator argument parser

--- gen_test_data.py
+++ gen_test_data.py
@@ -1,6 +1,29 @@
+The MIT License (MIT)
+
+Copyright (c) 2014 George Koodarappally
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 import string
 import random
 import argparse
+import sys
 
 
 MAX_COL_SIZE=65535
@@ -26,20 +49,21 @@ def gen_line(num_cols, col_size, delimiter):
     return row_data
     
 
-def gen_test_data(num_rows, num_cols, col_size, delimiter):
+def gen_test_data(output_file, num_rows, num_cols, col_size, delimiter):
     for i in range(num_rows):
         row_data =  gen_line(num_cols, col_size, delimiter)
-        print 'Line %s %s' % (i, row_data)
+        output_file.write(row_data+'\n')
 
 if __name__ =='__main__':
     parser = argparse.ArgumentParser(description='Generate test data for analyze_this.')
-    parser.add_argument('-r', '--rows', default=1000000, type=int, help='number of rows')
-    parser.add_argument('-c', '--columns', default=100, type=int, help='number of columns')
+    parser.add_argument('-r', '--rows', default=1000, type=int, help='number of rows')
+    parser.add_argument('-c', '--columns', default=10, type=int, help='number of columns')
     parser.add_argument('-s', '--column_size', default=255, type=int, help='max column size')
     parser.add_argument('-d', '--delimiter', default='|', help='column delimiter',
             choices=['|', ','])
+    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
     args = parser.parse_args()
 
-    print('Rows: %d Columns: %d Column-Size: %d Delimiter %s' % (args.rows, args.columns, args.column_size, args.delimiter))
+    #print('Rows: %d Columns: %d Column-Size: %d Delimiter %s' % (args.rows, args.columns, args.column_size, args.delimiter))
 
-    gen_test_data(args.rows, args.columns, args.column_size, args.delimiter)
+    gen_test_data(args.outfile, args.rows, args.columns, args.column_size, args.delimiter)
